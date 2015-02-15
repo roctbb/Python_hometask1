8ee4a46bbb7f2d6f40103a9562903391d88b8b13
Rostislav Borodin <roctbb@gmail.com>
2015-02-16 01:27:32 +0300

    correct

--- main.py
+++ main.py
@@ -11,41 +11,42 @@ for i in range(nodesnum):
     matrix[i][i] = 0
 rlist = dom.getElementsByTagName('resistor')
 for node in rlist:
-    fr = int(node.attributes["net_from"].nodeValue)-1
-    to = int(node.attributes["net_to"].nodeValue)-1
-    resis = float(node.attributes["resistance"].nodeValue)
+    fr = int(node.getAttribute("net_from"))-1
+    to = int(node.getAttribute("net_to"))-1
+    resis = float(node.getAttribute("resistance"))
     matrix[fr][to] = 1/(1/matrix[fr][to]+1/resis)
     matrix[to][fr] = 1/(1/matrix[to][fr]+1/resis)
-rlist = dom.getElementsByTagName('capator')
+rlist = dom.getElementsByTagName('capactor')
 for node in rlist:
-    fr = int(node.attributes["net_from"].nodeValue)-1
-    to = int(node.attributes["net_to"].nodeValue)-1
-    resis = float(node.attributes["resistance"].nodeValue)
+    fr = int(node.getAttribute("net_from"))-1
+    to = int(node.getAttribute("net_to"))-1
+    resis = float(node.getAttribute("resistance"))
     matrix[fr][to] = 1/(1/matrix[fr][to]+1/resis)
     matrix[to][fr] = 1/(1/matrix[to][fr]+1/resis)
 rlist = dom.getElementsByTagName('diode')
 for node in rlist:
-    fr = int(node.attributes["net_from"].nodeValue)-1
-    to = int(node.attributes["net_to"].nodeValue)-1
-    resis = float(node.attributes["resistance"].nodeValue)
-    revres = float(node.attributes["reverse_resistance"].nodeValue)
+    fr = int(node.getAttribute("net_from"))-1
+    to = int(node.getAttribute("net_to"))-1
+    resis = float(node.getAttribute("resistance"))
+    revres = float(node.getAttribute("reverse_resistance"))
     matrix[fr][to] = 1/(1/matrix[fr][to]+1/resis)
     matrix[to][fr] = 1/(1/matrix[to][fr]+1/revres)
 for k in range(nodesnum):
     for i in range(nodesnum):
         for j in range(nodesnum):
-            if matrix[i][j] == 0:
-                a = float('inf')
-            else:
-                a = 1/matrix[i][j]
-            if matrix[i][k]+matrix[k][j] == 0:
-                b = float('inf')
-            else:
-                b = 1/(matrix[i][k]+matrix[k][j])
-            if a+b == 0:
-                matrix[i][j] = float('inf')
-            else:
-                matrix[i][j] = 1/(a+b)
+            if i != j:
+                if matrix[i][j] == 0:
+                    a = float('inf')
+                else:
+                    a = 1/matrix[i][j]
+                if matrix[i][k]+matrix[k][j] == 0:
+                    b = float('inf')
+                else:
+                    b = 1/(matrix[i][k]+matrix[k][j])
+                if a+b == 0:
+                    matrix[i][j] = float('inf')
+                else:
+                    matrix[i][j] = 1/(a+b)
 wr = csv.writer(open(sys.argv[2], "w"), delimiter=',')
 for row in matrix:
     wr.writerow(row)
